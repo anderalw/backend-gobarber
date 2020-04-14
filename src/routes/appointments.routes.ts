a8e7444f293f393085aa41d7c7557593abef2047
Anderson <anderalw@gmail.com>
2020-04-14 16:14:24 -0300

    test

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,32 @@
+import { Router } from 'express';
+import {  parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreatAppointmetsServices from  '../services/CreateAppointmentService';
+
+const appointmentsRoutes = Router();
+const appointmentsRepository = new AppointmentsRepository();
+
+appointmentsRoutes.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.json(appointments);
+});
+
+appointmentsRoutes.post('/', (request, response) => {
+  try {
+    const { provider, date } = request.body;
+
+    const parsedDate = parseISO(date);
+
+    const creatAppointmets = new CreatAppointmetsServices( appointmentsRepository );
+
+    const appointment = creatAppointmets.execute({date: parsedDate, provider});
+
+    return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ error: err.message});
+  }
+});
+
+export default appointmentsRoutes;
