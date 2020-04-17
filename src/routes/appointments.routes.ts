a74c086ec937b5c94473760cc183260e4826b124
Anderson <anderalw@gmail.com>
2020-04-16 22:51:05 -0300

    update

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,29 +1,29 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
 import { parseISO } from 'date-fns';
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreatAppointmetsServices from '../services/CreateAppointmentService';
 
 const appointmentsRoutes = Router();
-const appointmentsRepository = new AppointmentsRepository();
 
-appointmentsRoutes.get('/', (request, response) => {
-  const appointments = appointmentsRepository.all();
+appointmentsRoutes.get('/', async (request, response) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+
+  const appointments = await appointmentsRepository.find();
 
   return response.json(appointments);
 });
 
-appointmentsRoutes.post('/', (request, response) => {
+appointmentsRoutes.post('/', async (request, response) => {
   try {
     const { provider, date } = request.body;
 
     const parsedDate = parseISO(date);
 
-    const creatAppointmets = new CreatAppointmetsServices(
-      appointmentsRepository,
-    );
+    const creatAppointmets = new CreatAppointmetsServices();
 
-    const appointment = creatAppointmets.execute({
+    const appointment = await creatAppointmets.execute({
       date: parsedDate,
       provider,
     });
