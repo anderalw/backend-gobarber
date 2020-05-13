f317d6ca6013df592fa0b69724b067f96b4a4dd9
Anderson L. Wiedemann <anderalw@gmail.com>
2020-05-12 21:19:33 -0300

    update

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express';
 
-import AuthencicateUserService from '../services/AuthenticateUserService';
+import AuthencicateUserService from '@modules/users/services/AuthenticateUserService';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
   const { email, password } = request.body;
 
-  const authenticateUser = new AuthencicateUserService();
+  const usersRepository = new UsersRepository();
+  const authenticateUser = new AuthencicateUserService(usersRepository);
 
   const { user, token } = await authenticateUser.execute({
     email,
