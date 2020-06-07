403cb9abf716e1ace1cc9e1808d4f2b72e4705a1
Anderson L. Wiedemann <anderalw@gmail.com>
2020-06-07 15:28:26 -0300

    Emails pelo Amazon SES

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'ander@andershome.com.br',
+      name: 'Anderson de casa',
+    },
+  },
+} as IMailConfig;
